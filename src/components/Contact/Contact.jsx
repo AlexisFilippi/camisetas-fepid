34a04b7070ccdfebda844c5a58e0e9ded3fea725
alexisfilippi <alexis.filippi23@gmail.com>
2024-02-08 00:15:31 -0300

    Ver detalles antes de entrega

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,23 +1,39 @@
 import { useForm } from "react-hook-form"
 import './Contact.css'
+import { addDoc, collection } from "firebase/firestore"
+import { db } from "../../firebase/config"
 
 const Contacto = () => {
 
-const { register, handleSubmit} = useForm()
-
-const enviar = (data) =>{
-console.log(data);
 
 
-}
+
+    const { register, handleSubmit, reset } = useForm()
+
+    const enviar = (data) => {
+        const consulta = {
+
+            consultas: data
+
+        }
+
+        const consultasRef = collection(db, "consultas")
+        addDoc(consultasRef, consulta)
+        reset()
+
+    }
+
+
+
+
 
     return (
         <div className="formContainer">
             <h2 className="tituloForm">Contacto</h2>
             <form className="formulario" onSubmit={handleSubmit(enviar)}>
-                <input type="text" placeholder="Ingresa tu nombre" {...register("nombre")}/>
+                <input type="text" placeholder="Ingresa tu nombre" {...register("nombre")} />
                 <input type="email" placeholder="Ingresa tu email" {...register("email")} />
-                <input type="consulta" placeholder="Ingresa tu consulta" {...register("consulta")}/>
+                <input type="consulta" placeholder="Ingresa tu consulta" {...register("consulta")} />
                 <button type="submit">Enviar</button>
             </form>
         </div>
